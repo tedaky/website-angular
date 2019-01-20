38941771931ff5c052e46a98f21d47d10c14edca
Speed Force <speedforce@The-Flash.local>
2019-01-20 14:38:58 -0600

    test(app): add test for branch two
    Add test for branch two coverage.

--- src/app/app.module.spec.ts
+++ src/app/app.module.spec.ts
@@ -1,24 +1,62 @@
 import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { PLATFORM_ID, APP_ID } from '@angular/core';
+
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
 
 describe('AppModule', () => {
-  let fixture: ComponentFixture<AppComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule,
-        AppModule
-      ]
-    }).compileComponents();
+  describe('Browser', () => {
+    let fixture: ComponentFixture<AppComponent>;
 
-    fixture = TestBed.createComponent(AppComponent);
-  }));
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        imports: [
+          RouterTestingModule,
+          AppModule
+        ],
+        providers: [
+          {
+            provide: PLATFORM_ID,
+            useValue: 'browser'
+          }
+        ]
+      }).compileComponents();
 
-  it('should create the app', () => {
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+      fixture = TestBed.createComponent(AppComponent);
+    }));
+
+    it('should create the app on browser', () => {
+      const app = fixture.debugElement.componentInstance;
+      console.log(APP_ID);
+      expect(app).toBeTruthy();
+    });
+  });
+
+  describe('Server', () => {
+    let fixture: ComponentFixture<AppComponent>;
+
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        imports: [
+          RouterTestingModule,
+          AppModule
+        ],
+        providers: [
+          {
+            provide: PLATFORM_ID,
+            useValue: 'server'
+          }
+        ]
+      }).compileComponents();
+
+      fixture = TestBed.createComponent(AppComponent);
+    }));
+
+    it('should create the app on server', () => {
+      const app = fixture.debugElement.componentInstance;
+      expect(app).toBeTruthy();
+    });
   });
 });
