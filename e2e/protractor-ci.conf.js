2781fe0e1fdf5e3dd45ce908d6909c97de041d1a
Speed Force <speedforce@The-Flash.local>
2019-01-20 21:13:08 -0600

    feat(ci): add travis, coveralls, circleci
    Add Travis and Coveralls. Update CircleCI.

---
+++ e2e/protractor-ci.conf.js
@@ -0,0 +1,16 @@
+// Protractor configuration file, see link for more information
+// https://github.com/angular/protractor/blob/master/lib/config.ts
+
+const config = require('./protractor.conf').config;
+
+config.capabilities = {
+  browserName: 'chrome',
+  chromeOptions: {
+    args: [
+      '--headless',
+      '--no-sandbox'
+    ]
+  }
+};
+
+exports.config = config;
