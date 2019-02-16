47ad21d2e6689deebd59eea01b41b23fbf6cc5bc
Speed Force <speedforce@The-Flash.local>
2019-02-16 10:45:07 -0600

    chore(types): add types
    Add types to root as client and server use same types.

---
+++ types/skill.d.ts
@@ -0,0 +1,24 @@
+export interface ISkillGroup {
+  skill_group_id: number;
+  skill_group_name: string;
+  skill_group_order: Date;
+  skill_group_modified_at: Date;
+}
+
+export interface ISkill {
+  skill_id: number;
+  skill_name: string;
+  skill_level: number;
+  skill_order: number;
+  skill_skill_group_id: number;
+  skill_modified_at: Date;
+}
+
+export interface ISkillResponse {
+  skill_group: ISkillGroup;
+  skill: ISkill[];
+}
+
+export interface SkillResponse {
+  response: ISkillGroup[];
+}
