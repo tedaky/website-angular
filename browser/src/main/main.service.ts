1d7a8d4052779df631400011e418ef3de5747ba6
Speed Force <speedforce@The-Flash.local>
2019-03-07 06:41:51 -0600

    build(main): switch to getter and setter

--- browser/src/main/main.service.ts
+++ browser/src/main/main.service.ts
@@ -16,9 +16,21 @@ import { Response } from '../../../types/message';
 export class MainService {
 
   /**
-   * API URL
+   * URL Holder
    */
-  private url: string;
+  private _url: string;
+  /**
+   * Get API URL
+   */
+  private get url(): string {
+    return this._url;
+  }
+  /**
+   * Set API URL
+   */
+  private set url(val: string) {
+    this._url = val;
+  }
 
   constructor(
     private httpClient: HttpClient
@@ -31,7 +43,7 @@ export class MainService {
    *
    * @returns `Observable<Response>`
    */
-  public getMessages(): Observable<Response> {
+  public get message(): Observable<Response> {
     return this.httpClient.get<Response>(this.url)
       .pipe<Response>(timeout<Response>(4000))
       .pipe<Response>(distinctUntilChanged<Response>());
